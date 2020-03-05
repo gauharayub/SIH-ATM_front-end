7385661353952a5cee7e95e07d25823b73782135
anas40 <anasbeg50@gmail.com>
2020-03-05 21:57:43 +0530

    table is fetching data

--- src/components/Tables/OrderedTable.vue
+++ src/components/Tables/OrderedTable.vue
@@ -3,7 +3,7 @@
     <div class="md-layout-item">
       <md-field>
         <label for="list">List</label>
-        <md-select v-if="items.legth" v-model="list" name="list" id="list">
+        <md-select v-model="list" name="list" id="list">
           <md-option value="3 month">3 month</md-option>
           <md-option value="6 month">6 month</md-option>
           <md-option value="yearly">yearly</md-option>
@@ -12,7 +12,7 @@
     </div>
     <b-table striped hover :fields="fields" :items="items">
       <template v-slot:cell(name)="data">
-        <router-link :to="`/joblist/${data.item.code}`">{{ data.value }}</router-link>
+        <router-link :to="`/joblist/${data.item.assignmentCode}`">{{ data.value }}</router-link>
       </template>
     </b-table>
   </div>
@@ -20,43 +20,45 @@
 
 <script>
 export default {
+  mounted() {
+    fetch("http://localhost:3000/get-orders", {
+      method: "get",
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(jsondata => {
+        this.items = jsondata
+        console.log(this.items)
+      })
+      .catch(er => {
+        console.log("Cant fetch the data:", er);
+        prompt("Check internet connection");
+      });
+  },
+
   data() {
     return {
-      mounted() {
-        console.log("Hello");
-        fetch("http://localhost:3000/get-orders", {
-          method: "get"
-        })
-          .then(response => {
-            console.log("Anas2");
-            return response.json();
-          })
-          .then(jsondata => {
-            this.items = jsondata.results;
-          })
-          .catch(er => {
-            console.log("Cant fetch the data:", er);
-            prompt("Check internet connection");
-          });
-        console.log("Anas");
-      },
       list: "3 month",
       fields: [
-        "id",
         {
           key: "name",
           label: "Code",
           formatter(value, key, item) {
-            return item.code;
+            return item.assignmentCode;
           }
         },
-        "Type",
-        "Description",
-        "Site",
-        "Status",
-        "Assets"
+        "completed",
+        "equipment",
+        {
+          key: "work",
+          label:"description"
+        },
+        "status",
+        "task",
+        "location"
       ],
-      items: [],
+      items: []
     };
   }
 };
