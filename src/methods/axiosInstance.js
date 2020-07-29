d33d15ad39a27bd649b3a73458525cb6ce60bf23
anas40 <anasbeg50@gmail.com>
2020-07-29 23:07:00 +0530

    fixed the https typo

--- src/methods/axiosInstance.js
+++ src/methods/axiosInstance.js
@@ -7,7 +7,7 @@ Vue.use(VueCookies);
 function axiosInstance() {
     console.log("Vue cookie", Vue.$cookies.get('token') || undefined)
     return axios.create({
-        baseURL: "http://ak11.herokuapp.com",
+        baseURL: "https://ak11.herokuapp.com",
         headers: { authorization: Vue.$cookies.get('token') || undefined }
     })
 }
